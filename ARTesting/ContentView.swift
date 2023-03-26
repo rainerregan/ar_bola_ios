f6c3cbc4c87a33b31838fed06712b772f3b974ac
Nuki Venoza <nukivenoza03@gmail.com>
2023-03-26 21:00:43 +0700

    edit home ui design and app icon,fix bowling bug

--- ARTesting/ContentView.swift
+++ ARTesting/ContentView.swift
@@ -17,26 +17,41 @@ struct ContentView: View {
         NavigationView {
             ZStack{
                 VStack {
-                    Text("AR Olympics")
-                        .font(.title)
-                        .foregroundColor(.black)
-                    HStack {
+                    
+                    Image("appTitle")
+                        .resizable()
+                        .frame(width: 362, height: 204)
+                                        
+                    VStack{
                         NavigationLink(destination: ARCameraView(type: .bowling)) {
                             Text("Bowling")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color(red: 0.0, green: 0.00784313725490196, blue: 0.16862745098039217, opacity: 100.0))
+                                .frame(maxWidth: .infinity, maxHeight: 24)
                                 .padding(20)
-                                .background(Color.orange)
-                                .foregroundColor(Color.white)
-                                .cornerRadius(10)
+                                .background(Color.white)
+                                .cornerRadius(30)
+                                .shadow(radius: 10)
                         }
+                        .padding(.bottom, 20)
+                        
                         NavigationLink(destination: ARCameraView(type: .soccer)) {
-                            Text("Bola")
+                            Text("Soccer")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color(red: 0.0, green: 0.00784313725490196, blue: 0.16862745098039217, opacity: 100.0))
+                                .frame(maxWidth: .infinity, maxHeight: 24)
                                 .padding(20)
-                                .background(Color.orange)
+                                .background(Color.white)
                                 .foregroundColor(Color.white)
-                                .cornerRadius(10)
+                                .cornerRadius(30)
+                                .shadow(radius: 10)
                         }
                     }
-                    
+                    .padding(.top, -50)
+                    .padding(20)
+                    .padding(.bottom, 100)
                 }
             }
             .background(
